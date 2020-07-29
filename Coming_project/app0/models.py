7e8afbeb2cfb714ded183d2fbec5ef29545182fd
lmj <minjung0067@naver.com>
2020-07-29 15:12:59 +0900

    수정

--- Coming_project/app0/models.py
+++ Coming_project/app0/models.py
@@ -19,3 +19,14 @@ class item(models.Model):
 
     class Meta:
         ordering = ['date']
+
+class User(models.Model):
+    username = models.CharField(max_length=64, verbose_name = '사용자명')
+    password = models.CharField(max_length=64, verbose_name = '비밀번호')
+    registered_dttm = models.DateTimeField(auto_now_add=True, verbose_name='등록시간')
+    def __str__(self):
+        return self.username
+
+        
+    class Meta:
+        db_table = 'test_user'
