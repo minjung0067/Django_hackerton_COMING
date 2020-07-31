82967157e7612c300f8c7304a712ddbf8d8e9e6b
minji <minji599@naver.com>
2020-07-31 21:09:14 +0900

    manage

--- Coming_project/coming_proj/urls.py
+++ Coming_project/coming_proj/urls.py
@@ -6,9 +6,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home, name='home'),
     path('main/',views.main,name='main'),
     path('secondmain/',views.secondmain,name='secondmain'),
-    path('',views.home, name='home'),
     path('loginhome/',views.loginhome,name='loginhome'),
     path('signuphome/',views.signuphome, name='signuphome'),
     path('logout/',views.logout, name='logout'),
