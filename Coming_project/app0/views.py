c526790f4a13a990412a6340dd25fc215b623b12
minjung <minjung0067@naver.com>
2020-07-31 23:18:09 +0900

    minjung_item

--- Coming_project/app0/views.py
+++ Coming_project/app0/views.py
@@ -7,13 +7,12 @@ from .forms import createForm
 from django.contrib import auth
 import os
 
-def main(request):
-    Item = item.objects.all()
-    return render(request,'main.html',{"main_key":Item})
-
 def secondmain(request):
     Item = item.objects.all()
     return render(request,'secondmain.html',{"main_key":Item})
+
+def main(request):
+    return render(request,'main.html')
 
 def detail(request,detail_id):
     Item_detail = get_object_or_404(item, pk=detail_id)
